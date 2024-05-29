f51a56cf91088a725d4088d500d7654a853abe3c
Kirill Belyaev <kirill@burblesoftware.com>
2024-05-29 12:38:53 +0500

    initial commit

---
+++ incomecalc/db_module.py
@@ -0,0 +1,106 @@
+# * Income Calculator
+# *
+# * Copyright (C) 2024 Kirill A Belyaev
+# *
+# * E-mail contact:
+# * kirillbelyaev@yahoo.com
+# *
+# * This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License.
+# * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/ or send
+# * a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+
+import datetime
+import sqlite3
+
+db_name = "calc.db"
+conn = sqlite3.connect(db_name)
+cur = conn.cursor()
+
+def create_db():
+    #conn = sqlite3.connect(db_name)
+    #cur = conn.cursor()
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS income(
+       id INT,
+       date TEXT,
+       sum INT);
+    """)
+    conn.commit()
+
+def showIncomeTbl():
+    cur.execute("SELECT * FROM income;")
+    one_result = cur.fetchall()
+    print("tbl: ", one_result)
+
+def showMonthlyIncomeTbl():
+    today = datetime.date.today()
+    current_year = today.year
+    current_month = today.strftime("%m")
+
+    cur.execute("SELECT date, sum FROM income WHERE date like '" + current_year.__str__() + "-" + current_month.__str__() + "-%';")
+    res = cur.fetchall()
+
+    return res
+
+
+def showMayMonthlySumIncomeTbl():
+    today = datetime.date.today()
+    current_year = today.year
+    cur.execute("SELECT sum(sum) FROM income WHERE date like '" + current_year.__str__() + "-05-%';")
+    res = cur.fetchone()
+
+    print("current year: ", current_year)
+    print("total sum: ", res)
+
+    return res
+
+def showMonthlySumIncomeTbl():
+    today = datetime.date.today()
+    current_year = today.year
+    current_month = today.strftime("%m")
+
+    cur.execute("SELECT sum(sum) FROM income WHERE date like '" + current_year.__str__() + "-" + current_month.__str__() + "-%';")
+    res = cur.fetchone()
+
+    print("today: ", today)
+    print("current year: ", current_year)
+    print("current month: ", current_month)
+    print("total sum: ", res)
+
+    return res
+
+
+def showIncomeTblRecord(dt):
+    cur.execute("SELECT sum AS sum FROM income where date = '" + dt + "';")
+    one_result = cur.fetchone()
+    print("tbl: ", one_result)
+
+def showIncomeTblCurrRecord():
+    current_date = datetime.date.today()
+
+    cur.execute("SELECT sum AS sum FROM income where date = '" + current_date.__str__() + "';" )
+    res = cur.fetchone()
+
+    print("sum: " + str(res) )
+
+    return res
+
+def createIncomeTblRecord(sum):
+    # get current date
+    current_date = datetime.date.today()
+
+    rec = (1, current_date.__str__(), sum)
+    cur.execute("INSERT INTO income VALUES(?, ?, ?);", rec)
+    conn.commit()
+    print("insert committed. ")
+
+
+def updateIncomeTblRecord(sum):
+    # get current date
+    current_date = datetime.date.today()
+    cur.execute("UPDATE income SET sum = " + sum + " WHERE date = '" + current_date.__str__() + "' ;")
+    conn.commit()
+    print("update committed. ")
+
+
+create_db()
---
+++ incomecalc/incomeCalc.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+
+# * Income Calculator
+# *
+# * Copyright (C) 2024 Kirill A Belyaev
+# *
+# * E-mail contact:
+# * kirillbelyaev@yahoo.com
+# *
+# * This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License.
+# * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/3.0/ or send
+# * a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+
+import wx
+
+from db_module import *
+
+class IncomeCalc(wx.Frame):
+
+    num = "";
+
+    def __init__(self, parent, title):
+        # ensure the parent's __init__ is called
+        super(IncomeCalc, self).__init__(parent, size=(220, 300))
+
+        # create a panel in the frame
+        panel = wx.Panel(self)
+
+        # # put some text with a larger bold font on it
+        st = wx.StaticText(panel, label="Income Tracker Software")
+        font = st.GetFont()
+        font.PointSize += 10
+        font = font.Bold()
+        st.SetFont(font)
+
+        # and create a sizer to manage the layout of child widgets
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(st, wx.SizerFlags().Border(wx.TOP | wx.LEFT, 5))
+        panel.SetSizer(sizer)
+
+        box = wx.BoxSizer(wx.VERTICAL)
+        labelTxt = wx.StaticText(panel, -1, "Daily Sum")
+
+        box.Add(labelTxt, 1, wx.ALIGN_LEFT, 5)
+        self.txtBox = wx.TextCtrl(panel)
+
+        box.Add(self.txtBox, 1, wx.ALIGN_LEFT, 5)
+        self.txtBox.Bind(wx.EVT_TEXT, self.OnKeyTyped)
+        sizer.Add(box)
+
+        button = wx.Button(panel, wx.ID_ANY, "Add Daily Sum \t", (100,100) )
+        #sizer.Add(button, 0, wx.ALIGN_LEFT)
+        sizer.Add(button)
+        button.Bind(wx.EVT_BUTTON, self.onButtonAddDailySum)
+
+        buttonR = wx.Button(panel, wx.ID_ANY, "Show Daily Sum ", (100, 100))
+        # sizer.Add(button, 0, wx.ALIGN_LEFT)
+        sizer.Add(buttonR)
+        buttonR.Bind(wx.EVT_BUTTON, self.onButtonShowDailySum)
+
+        buttonMT = wx.Button(panel, wx.ID_ANY, "Show \n Monthly Total \t", (100, 100))
+        # sizer.Add(button, 0, wx.ALIGN_LEFT)
+        sizer.Add(buttonMT)
+        buttonMT.Bind(wx.EVT_BUTTON, self.onButtonShowMonthlyTotal)
+
+        # create a menu bar
+        self.makeMenuBar()
+
+        # and a status bar
+        self.CreateStatusBar()
+        self.SetStatusText("Welcome to Income Tracker")
+
+        self.Centre()
+        self.Show()
+        self.Fit()
+
+    def makeMenuBar(self):
+        """
+        A menu bar is composed of menus, which are composed of menu items.
+        This method builds a set of menus and binds handlers to be called
+        when the menu item is selected.
+        """
+
+        # Make a file menu with Hello and Exit items
+        fileMenu = wx.Menu()
+        # The "\t..." syntax defines an accelerator key that also triggers
+        # the same event
+        funcItem = fileMenu.Append(
+            -1,
+            "Show Monthly Report",
+            "Show monthly report on all daily entries",
+        )
+        fileMenu.AppendSeparator()
+        # When using a stock ID we don't need to specify the menu item's
+        # label
+        exitItem = fileMenu.Append(wx.ID_EXIT)
+
+        # Now a help menu for the about item
+        helpMenu = wx.Menu()
+        aboutItem = helpMenu.Append(wx.ID_ABOUT)
+
+        # Make the menu bar and add the two menus to it. The '&' defines
+        # that the next letter is the "mnemonic" for the menu item. On the
+        # platforms that support it those letters are underlined and can be
+        # triggered from the keyboard.
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu, "&File")
+        menuBar.Append(helpMenu, "&Help")
+
+        # Give the menu bar to the frame
+        self.SetMenuBar(menuBar)
+
+        # Finally, associate a handler function with the EVT_MENU event for
+        # each of the menu items. That means that when that menu item is
+        # activated then the associated handler function will be called.
+        self.Bind(wx.EVT_MENU, self.showMonthlyReport, funcItem)
+        self.Bind(wx.EVT_MENU, self.OnExit, exitItem)
+        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+
+    def OnExit(self, event):
+        """Close the frame, terminating the application."""
+        self.Close(True)
+
+    def showMonthlyReport(self, event):
+        """Say hello to the user."""
+        r = showMonthlyIncomeTbl()
+
+        if r is not None:
+            wx.MessageBox("Monthly list: \n" + r.__str__().strip('[]').replace("),", ")\n"))
+        else:
+            wx.MessageBox("No data available")
+
+    def OnAbout(self, event):
+        """Display an About Dialog"""
+        wx.MessageBox(
+            "Income Calculator version 0.1",
+            "About Income Calculator",
+            wx.OK | wx.ICON_INFORMATION,
+        )
+
+    def onButtonAddDailySum(self, event):
+        if self.num.isnumeric() == False:
+            wx.MessageBox("Num Error!")
+        else:
+            n = showIncomeTblCurrRecord()
+
+            if n is None:
+                createIncomeTblRecord(self.num)
+            else:
+                updateIncomeTblRecord(self.num)
+
+    def onButtonShowDailySum(self, event):
+        n = showIncomeTblCurrRecord()
+
+        if n is not None:
+            wx.MessageBox("Daily sum: " + str(n[0]))
+
+    def onButtonShowMonthlyTotalMay(self, event):
+        n = showMayMonthlySumIncomeTbl()
+
+        if n is not None:
+            wx.MessageBox("May total: " + str(n[0]))
+
+    def onButtonShowMonthlyTotal(self, event):
+        n = showMonthlySumIncomeTbl()
+
+        if n is not None:
+            wx.MessageBox("Monthly total: " + str(n[0]))
+
+    def OnKeyTyped(self, event):
+        self.num = event.GetString()
+        #print (event.GetString() )
+        print (self.num)
+
+
+if __name__ == "__main__":
+    # When this module is run (not imported) then create the app, the
+    # frame, show it, and start the event loop.
+    app = wx.App()
+    IncomeCalc(None, title="Income Calculator")
+    app.MainLoop()
